5d0347e92c2d28ce03bd00d6657bb408ec762d0b
Alina Gilmutdinova <a_zaynutdinova@bk.ru>
2021-08-14 18:44:21 +0300

    third commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,23 @@
-function compareArrays(arr1, arr2) {
-  let result;
+//Комментарий: 1 задание сделала ка на разборном занятии 
+
+
+const compareArrays = (a, b) => a.length === b.length && a.every((n, i) => n === b[i]);  //сравнить 2 массива
+  
+  //true && true && false && a = 10 + 5
+  //if(a > 0) a * a +10;
+  //a >0 && (a - a + 10)
+  //let result;
 
   // Ваш код
+//if(arr1.lenght !== arr2.lenght) return false;//если длины массивов не равны, то сразуfalse
+  //arr1.every((item, idx) => {
+  // return item === arr2[idx] //cравниваем равен ли индекс одного массива с другим
+ // })
+  //return result; // boolean
 
-  return result; // boolean
-}
-
+//Задание 2
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item) => (item > 3)).filter(item => (item % 3 === 0)).map(item => item * 10)
+  
   return resultArr; // array
 }
