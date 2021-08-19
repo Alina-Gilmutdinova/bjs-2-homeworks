9cdabee61b546994ad0c4a6a40bd7d23413939be
Alina Gilmutdinova <a_zaynutdinova@bk.ru>
2021-08-19 11:35:45 +0300

    второй вариант дз

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,27 +8,29 @@ const student1 = new Student();
 const student2 = new Student();
 
 Student.prototype.setSubject = function (subjectName) {
-  console.log("предмет:" + " " + this.subject);
+  this.subjectName = subjectName;
     //ваш код
 }
 
 // ваш код для остальных методов
 Student.prototype.addMark = function (mark) {
-  if(this.marks === undefined){ 
-    student1.addMark(5);// добавить первую оценку 
-    } else {
-      student1.addMark(4);
-      student1.addMark(3);
-      student1.addMark(2);
-      student1.addMark(1);// добавить вторую и последующие оценки в массив
+  this.mark = mark {
+    if(this.marks === undefined){ 
+      mark.push (5)// добавить первую оценку 
+      } else {
+        this.addMark.push(4);
+        this.addMark.push(3);
+        this.addMark.push(2);
+        this.addMark.push(1);// добавить вторую и последующие оценки в массив
+  }  
     }
-    console.log ("оценка:" + " " + this.mark);
 }
 
-Student.prototype.addMarks = function (mark1, mark2, mark3...) {
- console.log(marks); //ваш код
+Student.prototype.addMarks = function (mark1, mark2, mark3,...) {
+ addMarks.push(mark1, mark2, mark3, ...); //ваш код
 }
 
 Student.prototype.getAverage = function (avg) {
-  
+  let sum = 
+  return avg = sum / mark.length;
 }
