a7e9a2f69cdf581eb2bf1c1eb8c32ef00c745faf
Alina Gilmutdinova <a_zaynutdinova@bk.ru>
2021-08-22 09:27:59 +0300

    пятый вариант дз

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,7 +3,7 @@ function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
-}
+};
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
@@ -13,12 +13,12 @@ Student.prototype.setSubject = function (subjectName) {
 // ваш код для остальных методов
 Student.prototype.addMark = function (mark) {   
     if(this.marks === undefined) { // проверяем, получал ли наш студент оценки ранее
-      this.marks = [];// добавить первую оценку // если нет, создаём новый массив для их хранения
+      this.marks = [mark];// добавить первую оценку // если нет, создаём новый массив для их хранения
       } else {
         this.marks.push(mark); //добавить вторую и последующие оценки в массив
   };
     };
-}
+
 
 Student.prototype.addMarks = function (...mark) {
   if(this.marks === undefined) {
@@ -27,14 +27,16 @@ Student.prototype.addMarks = function (...mark) {
   } else {
   this.marks.push(...mark);
   };
-}
+};
 
 Student.prototype.getAverage = function () {
   return this.marks.reduce((acc, item) => acc += item) / this.marks.length;
 };
 
 Student.prototype.exclude = function (reason) {
-delete this.subject;
+
+delete this.subject; //удаляем объект 
 delete this.marks;
-this.exclude = reason;
+
+this.excluded = reason; //присваиваем новое значение
 }
